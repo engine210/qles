76e304050510f9941ebf44589f9fbc481cb69a53
Stevn Wang <engine210@gmail.com>
2021-04-22 17:56:24 +0800

    add remove script

---
+++ remove_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script is for removing all stuff install using main.sh
+# This is just for developing convenient
+
+rm -r $HOME/.oh-my-zsh
+rm $HOME/.zsh*
+rm $HOME/.tmux.conf
+rm $HOME/.vimrc
+rm $HOME/.bash_profile
+rm $HOME/.qles*
